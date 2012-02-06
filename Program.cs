538e4f5af9cea30ea65c3cdcd2061d99f7b68e91
Einar Egilsson <einar@einaregilsson.com>
2012-02-06 17:10:24 +0100

    Pulled main code into its own class

--- Program.cs
+++ Program.cs
@@ -12,18 +12,9 @@ namespace EinarEgilsson.Utilities.Win32Icons
     {
         static void Main(string[] args)
         {
-            string file = @"..\..\..\IconExe\bin\release\IconExe.exe";
-            //IconDirectoryResource r1 = new IconDirectoryResource();
-            //r1.LoadFrom(file);
-            IconDirectoryResource res = new IconDirectoryResource(new IconFile(@"C:\users\einar\desktop\sec.ico"));
-            res.Name.Id = (IntPtr)1;
-            res.Name.Name = "John";
-            res.SaveTo(file);
-            res = new IconDirectoryResource(new IconFile(@"C:\users\einar\desktop\cmd.ico"));
-            res.Name.Name = "Foo";
-            res.Name.Id = (IntPtr)2;
-            res.SaveTo(file);
- 
+            string file = args[0];
+            var winIconFile = new WinIconAssembly(file);
+            winIconFile.AddIcon(args[1]);
         }
     }
 }
---
+++ WinIconAssembly.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vestris.ResourceLib;
+
+namespace EinarEgilsson.Utilities.Win32Icons
+{
+    public class WinIconAssembly
+    {
+        public string Filename { get; private set; }
+
+        public WinIconAssembly(string assemblyFilename)
+        {
+            Filename = assemblyFilename;
+        }
+
+        public void AddIcon(string iconFilename)
+        {
+            IconDirectoryResource res = new IconDirectoryResource(new IconFile(iconFilename));
+            res.SaveTo(Filename);
+        }
+    }
+}
