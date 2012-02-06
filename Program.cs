4e1ba4f5b3464ac0209a5edd30644ff80d96017f
Einar Egilsson <einar@einaregilsson.com>
2012-02-06 16:45:58 +0100

    Initial files

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vestris.ResourceLib;
+using System.Reflection;
+using System.IO;
+
+namespace ResourceIcons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"..\..\..\IconExe\bin\release\IconExe.exe";
+            //IconDirectoryResource r1 = new IconDirectoryResource();
+            //r1.LoadFrom(file);
+            IconDirectoryResource res = new IconDirectoryResource(new IconFile(@"C:\users\einar\desktop\sec.ico"));
+            res.Name.Id = (IntPtr)1;
+            res.Name.Name = "John";
+            res.SaveTo(file);
+            res = new IconDirectoryResource(new IconFile(@"C:\users\einar\desktop\cmd.ico"));
+            res.Name.Name = "Foo";
+            res.Name.Id = (IntPtr)2;
+            res.SaveTo(file);
+ 
+        }
+    }
+}
