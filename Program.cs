ebd6eba7dcb7e52de4d8c88578fec6fc95daf750
Einar Egilsson <einar@einaregilsson.com>
2012-02-09 12:47:04 +0100

    Fix formatting

--- Program.cs
+++ Program.cs
@@ -187,28 +187,28 @@ more information about this program and how to use it.
 Usage: InsertIcons <assemblyfile> <icons>
 
 <assemblyfile>    A .NET assembly (or any PE file really) that you want
-                    to add icons to.
+                  to add icons to.
 
 <icons>           The <icons> parameter can accept 4 different types
-                    of arguments:
+                  of arguments:
                   
-                    1. It can be a list of .ico files seperated by ;, e.g.
-                        icon1.ico;icon2.ico;icon3.ico.
+                  1. It can be a list of .ico files seperated by ;, e.g.
+                     icon1.ico;icon2.ico;icon3.ico.
                      
-                    2. It can be a directory name, in which case all .ico
-                        files in the directory will be added, in 
-                        alphabetical order.
+                  2. It can be a directory name, in which case all .ico
+                     files in the directory will be added, in 
+                     alphabetical order.
 
-                    3. It can be the name of a text file which contains a
-                        list of icons to add to the assembly. The file
-                        should simply have one .ico file path on each line.
+                  3. It can be the name of a text file which contains a
+                     list of icons to add to the assembly. The file
+                     should simply have one .ico file path on each line.
    
-                    4. It can be left out completely, in which case the
-                        program reads filenames from the standard input
-                        stream. This allows for filenames to be piped into
-                        the program, e.g. 
+                  4. It can be left out completely, in which case the
+                     program reads filenames from the standard input
+                     stream. This allows for filenames to be piped into
+                     the program, e.g. 
                           
-                            dir /b /s *ico | InsertIcons myfile.exe
+                         dir /b /s *ico | InsertIcons myfile.exe
 
 ");
         }
