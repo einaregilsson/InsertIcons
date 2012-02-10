18a8f13ab272966f3e9b80542b83564d5528f785
Einar Egilsson <einar@einaregilsson.com>
2012-02-10 08:40:29 +0100

    Strong name signed test app

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace TestApp
@@ -9,6 +10,7 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("My name is " + Assembly.GetExecutingAssembly().GetName());
         }
     }
 }
